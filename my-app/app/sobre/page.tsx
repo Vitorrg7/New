200f9adc2b4c76727328bcce6107a6419a5f3886
Vitorrg7 <vitorgabrielgb6@gmail.com>
2026-08-20 15:51:49 -0300

    first-page

---
+++ my-app/app/sobre/page.tsx
@@ -0,0 +1,8 @@
+export default function Sobre() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Sobre Nós</h1>
+      <p>Esta é a minha nova página criada no Next.js!</p>
+    </main>
+  );
+}
